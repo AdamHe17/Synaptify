cd70f611f4c17174181325ec89da770a40ce39e1
Eric Hao <Eric@Erics-MacBook-Pro.local>
2016-02-27 10:27:26 -0800

    Moved python script out of samples folder and into main director

--- matlab_to_python.py
+++ matlab_to_python.py
@@ -1,7 +1,7 @@
 import matlab.engine
 
 eng = matlab.engine.start_matlab()
-eng.cd(r'C:\Users\ah299_000\Documents\GitHub\Synaptify')
+eng.cd(r'~/Documents/myStuff/HackTech2016/Synaptify')
 outputs = eng.analysis('identify_2016-02-26_20-37-59_00.bmp',nargout=4)
 
 ridges, bifurcations = [], []
