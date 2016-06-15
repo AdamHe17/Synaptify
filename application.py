5336b7be172e73ca480f02861dbe2098936e4f11
Max Schuman <maxschuman2018@u.northwestern.edu>
2016-06-14 21:25:51 -0400

    Some other stuff idk

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-@app.route("/")
+application = Flask(__name__)
+@application.route("/")
 
-def main():
+def index():
 	return render_template('index.html')
 
 if __name__=="__main__":
-	app.run()
+	application.run()
 
